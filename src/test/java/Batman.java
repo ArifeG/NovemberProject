205dfea6261892693b904f3cd1ddc26050a701c8
Arife Gevrek <arifeakyol34@gmail.com>
2020-11-01 11:24:08 -0500

    Batman has Bat bike

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -2,7 +2,7 @@ import jdk.internal.org.objectweb.asm.tree.MultiANewArrayInsnNode;
 
 public class Batman {
     public static void main(String[] args) {
-        System.out.println("Hi world,how are you,ok");
+        System.out.println("Hi world,how are you,ok or not");
 
     }
 }
