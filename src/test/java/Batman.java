47a8fb590d890a1fe66d1f0796c1167bc8ba004f
Arife Gevrek <arifeakyol34@gmail.com>
2020-11-01 11:20:19 -0500

    Batman says more more

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -2,7 +2,7 @@ import jdk.internal.org.objectweb.asm.tree.MultiANewArrayInsnNode;
 
 public class Batman {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Hi world");
 
     }
 }
